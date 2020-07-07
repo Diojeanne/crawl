Diojeanne/crawl

commit a7dbce93e49db5d09335ca7aa05b624713964b15

Remove some amulet attunement
"acrobat and "reflect no longer need 'attunement' logic, since they
take 5+5 turns to swap. "gspirit doesn't need to drain mp, either
(except for deep dwarves).

"regen and "mpreg probably don't need the attunement logic either,
but I'm unsure - is there some hypothetically optimal play that
involves swapping them while resting...?

crawl-ref/source/player-equip.cc
@@ -745,13 +745,13 @@ static void _spirit_shield_message(bool unmeld)
 {
     if (!unmeld && you.spirit_shield() < 2)
     {
-        dec_mp(you.magic_points);
         mpr("You feel your power drawn to a protective spirit.");
 #if TAG_MAJOR_VERSION == 34
         if (you.species == SP_DEEP_DWARF
             && !(have_passive(passive_t::no_mp_regen)
                  || player_under_penance(GOD_PAKELLAS)))
         {
+            dec_mp(you.magic_points);
             mpr("Now linked to your health, your magic stops regenerating.");
         }
 #endif
@@ -1132,20 +1132,6 @@ static void _equip_regeneration_item(const item_def &item)
     return;
 }
 
-static void _equip_amulet_of_the_acrobat()
-{
-    if (you.hp == you.hp_max)
-    {
-        you.activated.set(EQ_AMULET);
-        mpr("You feel ready to tumble and roll out of harm's way.");
-    }
-    else
-    {
-        you.activated.set(EQ_AMULET, false);
-        mpr("Your injuries prevent the amulet from attuning itself.");
-    }
-}
-
 bool acrobat_boost_active()
 {
     return you.activated[EQ_AMULET]
@@ -1162,7 +1148,6 @@ static void _equip_amulet_of_mana_regeneration()
     else if (you.magic_points == you.max_magic_points)
     {
         you.props[MANA_REGEN_AMULET_ACTIVE] = 1;
-        mpr("The amulet hums as it attunes itself to your energized body.");
     }
     else
     {
@@ -1173,17 +1158,8 @@ static void _equip_amulet_of_mana_regeneration()
 
 static void _equip_amulet_of_reflection()
 {
-    if (you.hp == you.hp_max)
-    {
-        you.activated.set(EQ_AMULET);
-        you.redraw_armour_class = true;
-        mpr("You feel a shielding aura gather around you.");
-    }
-    else
-    {
-        you.activated.set(EQ_AMULET, false);
-        mpr("The amulet cannot attune itself to your injured body.");
-    }
+    you.redraw_armour_class = true;
+    mpr("You feel a shielding aura gather around you.");
 }
 
 static void _equip_jewellery_effect(item_def &item, bool unmeld,
@@ -1268,7 +1244,7 @@ static void _equip_jewellery_effect(item_def &item, bool unmeld,
 
     case AMU_ACROBAT:
         if (!unmeld)
-            _equip_amulet_of_the_acrobat();
+            mpr("You feel ready to tumble and roll out of harm's way.");
         break;
 
     case AMU_MANA_REGENERATION:

crawl-ref/source/player.cc
@@ -2213,8 +2213,7 @@ int player_shield_class()
 
     shield += qazlal_sh_boost() * 100;
     shield += tso_sh_boost() * 100;
-    shield += you.activated[EQ_AMULET] * you.wearing(EQ_AMULET, AMU_REFLECTION)
-              * AMU_REFLECT_SH * 100;
+    shield += you.wearing(EQ_AMULET, AMU_REFLECTION) * AMU_REFLECT_SH * 100;
     shield += you.scan_artefacts(ARTP_SHIELDING) * 200;
 
     return (shield + 50) / 100;
@@ -5510,7 +5509,7 @@ bool player::shielded() const
            || duration[DUR_DIVINE_SHIELD]
            || get_mutation_level(MUT_LARGE_BONE_PLATES) > 0
            || qazlal_sh_boost() > 0
-           || you.wearing(EQ_AMULET, AMU_REFLECTION) && you.activated[EQ_AMULET]
+           || you.wearing(EQ_AMULET, AMU_REFLECTION)
            || you.scan_artefacts(ARTP_SHIELDING);
 }
 

crawl-ref/source/player-reacts.cc
@@ -840,29 +840,10 @@ static void _handle_emergency_flight()
     }
 }
 
-// Regen equipment and amulet of the acrobat only begins to function when full
-// health is reached while they are worn.
+// Regen equipment only begins to function when full health is reached.
 static void _update_equipment_attunement_by_health()
 {
-    if (you.hp != you.hp_max)
-        return;
-
-    if (!you.activated[EQ_AMULET] && you.wearing(EQ_AMULET, AMU_ACROBAT))
-    {
-        mprf("Your amulet attunes itself to your body. You feel like doing "
-             "cartwheels.");
-        you.activated.set(EQ_AMULET);
-    }
-
-    if (!you.activated[EQ_AMULET] && you.wearing(EQ_AMULET, AMU_REFLECTION))
-    {
-        mprf("Your amulet attunes itself to your body. You feel a shielding "
-             "aura gather around you.");
-        you.activated.set(EQ_AMULET);
-        you.redraw_armour_class = true;
-    }
-
-    if (you.get_mutation_level(MUT_NO_REGENERATION))
+    if (you.hp != you.hp_max || you.get_mutation_level(MUT_NO_REGENERATION))
         return;
 
     vector<string> eq_list;

commit 4e6e3e941dcfb448fffebdf3f9c52d4c9684fb11

Fix amulet of the acrobat
Broken in a7dbce93e49.

crawl-ref/source/player-equip.cc
@@ -1134,8 +1134,7 @@ static void _equip_regeneration_item(const item_def &item)
 
 bool acrobat_boost_active()
 {
-    return you.activated[EQ_AMULET]
-           && you.wearing(EQ_AMULET, AMU_ACROBAT)
+    return you.wearing(EQ_AMULET, AMU_ACROBAT)
            && you.duration[DUR_ACROBAT]
            && (!you.caught())
            && (!you.is_constricted());

crawl-ref/source/player.cc
@@ -1950,7 +1950,7 @@ bool player_is_shapechanged()
 
 void update_acrobat_status()
 {
-    if (!you.wearing(EQ_AMULET, AMU_ACROBAT) || !you.activated[EQ_AMULET])
+    if (!you.wearing(EQ_AMULET, AMU_ACROBAT))
         return;
 
     // Acrobat duration goes slightly into the next turn, giving the